586f58627be10311ab220f500120c00196aa1d5a
emilowhits <50845354+emilowhits@users.noreply.github.com>
2024-10-31 21:11:23 +0100

    Nyt script timer
    Start på win cindition, nåede ikke længere i dag

---
+++ SemesterProjekt Spildesign E24/Assets/Scripts/Win Condition/Timer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+
+    [SerializeField] TextMeshProUGUI timerText;
+ 
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
