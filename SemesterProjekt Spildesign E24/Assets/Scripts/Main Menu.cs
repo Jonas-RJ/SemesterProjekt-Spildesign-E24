2fb2d6b4c8c95438ce47bcb7239b8c51c23d90cf
emilowhits <50845354+emilowhits@users.noreply.github.com>
2024-11-19 17:06:33 +0100

    Added en Main menu

---
+++ SemesterProjekt Spildesign E24/Assets/Scripts/Main Menu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //Loader vores spil scene
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("Emil Scene"); //Burde være "Game Scene" i fremtiden
+    }
+
+    //Loader Main Menu scene
+    public void HowToPlayButton()
+    {
+        //SceneManager.LoadScene("How To Play Scene");
+    }
+}
