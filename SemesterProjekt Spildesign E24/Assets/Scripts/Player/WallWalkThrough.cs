4248dc1d4c036a98ade2e5056a83eac9a2a980a0
happy_Vergil <Ladingkenny@gmail.com>
2024-12-01 03:18:38 +0100

    væggerne og dør animation
    væggerne scripts og dør animation

--- SemesterProjekt Spildesign E24/Assets/Scripts/Player/WallWalkThrough.cs
+++ SemesterProjekt Spildesign E24/Assets/Scripts/Player/WallWalkThrough.cs
@@ -11,7 +11,7 @@ public class WallWalkThrough : MonoBehaviour
     public GameObject Player2;
     public GameObject WallOpen;
     public GameObject WallClose;
-
+    public Animator _Animator;
     public AudioSource wallGoThrough;
 
 
@@ -20,18 +20,36 @@ public class WallWalkThrough : MonoBehaviour
     {
         
     }
+    private void Awake()
+    {
+        _Animator = GetComponentInParent<Animator>();
+    }
+    private void FixedUpdate()
+    {
+        WallAnimator();
+    }
 
+    private void WallAnimator()
+    {
+        bool DoorOpen = WallOpen.activeSelf;
+
+        if (_Animator != null)
+        {
+            _Animator.SetBool("DoorOpen", DoorOpen);
+        }
+        
+    }
 
     // efter man g�r ud af en v�g, start coroutine
-  /* void OnTriggerEnter2D(Collider2D other)
-    {
-        if (Player2.layer == 7)
-        {
-            print("ran through wall lmao");
-            StartCoroutine(WallCoolDown());
-        }
-    }
-  */
+    /* void OnTriggerEnter2D(Collider2D other)
+      {
+          if (Player2.layer == 7)
+          {
+              print("ran through wall lmao");
+              StartCoroutine(WallCoolDown());
+          }
+      }
+    */
 
     private bool check1 = false;
     private bool check2 = false;
